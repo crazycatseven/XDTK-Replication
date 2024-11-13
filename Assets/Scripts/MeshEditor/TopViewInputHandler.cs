f70434b1e5d4574f45d54097728f3b4d531464e5
crazycatseven <80610581+crazycatseven@users.noreply.github.com>
2024-11-13 01:33:06 -0500

    Enable screen touch

--- Assets/Scripts/MeshEditor/TopViewInputHandler.cs
+++ Assets/Scripts/MeshEditor/TopViewInputHandler.cs
@@ -7,6 +7,7 @@ public class TopViewInputHandler : MonoBehaviour
 
     private bool isDraggingAxis = false;
     private Vector2 startMousePosition;
+    private Vector2 lastTouchPosition;
 
     void Start()
     {
@@ -22,11 +23,90 @@ public class TopViewInputHandler : MonoBehaviour
 
         if (selectionLogic.IsEnabled())
         {
-            HandleSelectionInput();
+            // 检测是触摸输入还是鼠标输入
+            if (Input.touchCount > 0)
+            {
+                HandleTouchInput();
+            }
+            else
+            {
+                HandleMouseInput();
+            }
         }
     }
 
+    void HandleTouchInput()
+    {
+        // 单指操作 - 对应鼠标左键
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
 
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startMousePosition = touch.position;
+                    isDraggingAxis = selectionLogic.GetComponent<TopViewGizmoController>().HandleAxisDrag(touch.position);
+                    if (!isDraggingAxis)
+                    {
+                        selectionLogic.UpdateSelection(startMousePosition, touch.position);
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (isDraggingAxis)
+                    {
+                        selectionLogic.GetComponent<TopViewGizmoController>().UpdateDrag(touch.position);
+                    }
+                    else
+                    {
+                        selectionLogic.UpdateSelection(startMousePosition, touch.position);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    if (isDraggingAxis)
+                    {
+                        selectionLogic.GetComponent<TopViewGizmoController>().EndDrag();
+                        isDraggingAxis = false;
+                    }
+                    else if (Vector2.Distance(startMousePosition, touch.position) <= 1.0f)
+                    {
+                        selectionLogic.HandleSingleClick(touch.position);
+                    }
+                    else
+                    {
+                        selectionLogic.EndSelection(startMousePosition, touch.position);
+                    }
+                    break;
+            }
+        }
+        // 双指操作 - 对应鼠标右键
+        else if (Input.touchCount == 2)
+        {
+            Touch touch1 = Input.GetTouch(0);
+            Touch touch2 = Input.GetTouch(1);
+            Vector2 touchCenter = (touch1.position + touch2.position) / 2;
+
+            if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
+            {
+                lastTouchPosition = touchCenter;
+            }
+            else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            {
+                Vector2 delta = touchCenter - lastTouchPosition;
+                selectionLogic.GetComponent<TopViewMapRenderer>().TopViewSelectionPanel.anchoredPosition += delta;
+                lastTouchPosition = touchCenter;
+            }
+        }
+    }
+
+    void HandleMouseInput()
+    {
+        HandleSelectionInput();
+    }
+
+    // 将原来的Update中的鼠标输入逻辑移到这个方法中
     void HandleSelectionInput()
     {
         // Mouse left button down
